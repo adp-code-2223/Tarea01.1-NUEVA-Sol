ca97438602756df5fe0b11cee8c0452d69a18702
mfernandez <mfernandez@192.168.56.1>
2022-10-11 21:14:55 +0200

    apartados a)-d)

--- src/ad/teis/UD1_Actividad13.java
+++ src/ad/teis/UD1_Actividad13.java
@@ -8,6 +8,7 @@ import ad.teis.model.Persona;
 import ad.teis.persistencia.DataIOPersistencia;
 import ad.teis.persistencia.IPersistencia;
 import ad.teis.persistencia.RandomAccessPersistencia;
+import java.util.ArrayList;
 
 /**
  *
@@ -20,22 +21,46 @@ public class UD1_Actividad13 {
     /**
      * @param args the command line arguments
      */
-
     public static void main(String[] args) {
-        IPersistencia dataIOPersistencia = new DataIOPersistencia();
-        Persona persona = new Persona(1, "12345678A", 18, 20000.65f);
-//        
-//        dataIOPersistencia.escribirPersona(persona, PERSONA_FILE);
-//        
-//      Persona personaRecuperada=  dataIOPersistencia.leerDatos(PERSONA_FILE);
-//      System.out.println("Se ha recuperado: " + personaRecuperada);
+        ArrayList<Persona> personas = new ArrayList<>();
+        ArrayList<Persona> personasRecuperadas = new ArrayList<>();
+        Persona personaA = new Persona(1, "12345678A", 18, 20000.65f);
+        Persona personaB = new Persona(2, "12345678B", 12, 30000.65f);
+        Persona personaC = new Persona(3, "12345678C", 22, 40000.65f);
+        personas.add(personaA);
+        personas.add(personaB);
+        personas.add(personaC);
 
-        IPersistencia random = new RandomAccessPersistencia();
+        RandomAccessPersistencia random = new RandomAccessPersistencia();
 
-        random.escribirPersona(persona, PERSONA_FILE);
-        Persona personaRecuperada = random.leerDatos(PERSONA_FILE);
+        //  random.escribirPersonas(personas, PERSONA_FILE);
+        personasRecuperadas = random.leerTodo(PERSONA_FILE);
 
-        System.out.println("La persona recuperada es: " + personaRecuperada);
+        int contador = 1;
+        for (Persona p : personasRecuperadas) {
+            System.out.println("Persona recuperada " + contador + ": " + p);
+            contador++;
+        }
+
+        int pos = 5;
+        Persona personaRecuperada = random.leerPersona(pos, PERSONA_FILE);
+        if (personaRecuperada != null) {
+            System.out.println("La persona en la posición: " + pos + " es: " + personaRecuperada);
+        }
+        else{
+             System.out.println("La persona en la posición: " + pos + " es null. Puede que no haya objetos de tipo Persona en esa posición.");
+        }
+        
+//        Persona personaZ = new Persona(100, "12345678Z", 23, 10000.5f);
+//       
+//        pos=25;
+//        Persona personaRecuperadaZ = random.add(pos, PERSONA_FILE, personaZ);
+//       if (personaRecuperadaZ != null) {
+//            System.out.println("La persona añadida en la posición: " + pos + " es: " + personaRecuperadaZ);
+//        }
+//        else{
+//             System.out.println("No se ha añadido la persona en la posición: " + pos);
+//        }
 
     }
 
--- src/ad/teis/persistencia/RandomAccessPersistencia.java
+++ src/ad/teis/persistencia/RandomAccessPersistencia.java
@@ -5,15 +5,21 @@
 package ad.teis.persistencia;
 
 import ad.teis.model.Persona;
+import java.io.EOFException;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author mfernandez
  */
 public class RandomAccessPersistencia implements IPersistencia {
+
+    private static final int LONG_BYTES_PERSONA = 34;
 
     @Override
     public void escribirPersona(Persona persona, String ruta) {
@@ -70,4 +76,140 @@ public class RandomAccessPersistencia implements IPersistencia {
 
     }
 
+    public void escribirPersonas(ArrayList<Persona> personas, String ruta) {
+        long longitudBytes = 0;
+        if (personas != null) {
+            try (
+                     RandomAccessFile raf = new RandomAccessFile(ruta, "rw");) {
+
+                longitudBytes = raf.length();
+                raf.seek(longitudBytes);
+                for (Persona persona : personas) {
+                    raf.writeLong(persona.getId());
+                    StringBuilder sb = new StringBuilder(persona.getDni());
+                    sb.setLength(9);
+                    raf.writeChars(sb.toString());
+                    //raf.writeUTF(sb.toString());
+
+                    raf.writeInt(persona.getEdad());
+                    raf.writeFloat(persona.getSalario());
+                }
+
+            } catch (FileNotFoundException ex) {
+                ex.printStackTrace();
+                System.out.println("Se ha producido una excepción: " + ex.getMessage());
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                System.out.println("Se ha producido una excepción: " + ex.getMessage());
+            }
+        }
+
+    }
+
+    public ArrayList<Persona> leerTodo(String ruta) {
+        long id ;
+        String dni ;
+        int edad;
+        float salario;
+        StringBuilder sb = new StringBuilder();
+        Persona persona = null;
+        ArrayList<Persona> personas = new ArrayList<>();
+        try (
+                 RandomAccessFile raf = new RandomAccessFile(ruta, "r");) {
+
+            do {
+                id = raf.readLong();
+                sb = new StringBuilder();
+                for (int i = 0; i <= 8; i++) {
+                    sb.append(raf.readChar());
+                }
+
+                dni = sb.toString();
+
+                edad = raf.readInt();
+                salario = raf.readFloat();
+
+                persona = new Persona(id, dni, edad, salario);
+                personas.add(persona);
+
+            } while (raf.getFilePointer() < raf.length());
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.out.println("Se ha producido una excepción: " + ex.getMessage());
+        }
+        return personas;
+
+    }
+
+    public Persona leerPersona(int posicion, String ruta) {
+        long id = 0;
+        String dni = "";
+        int edad = 0;
+        float salario = 0;
+        StringBuilder sb = new StringBuilder();
+        Persona persona = null;
+
+        try (
+                 RandomAccessFile raf = new RandomAccessFile(ruta, "r");) {
+
+            raf.seek(converToBytePosition(posicion));
+            id = raf.readLong();
+            for (int i = 0; i <= 8; i++) {
+                sb.append(raf.readChar());
+            }
+
+            dni = sb.toString();
+
+            edad = raf.readInt();
+            salario = raf.readFloat();
+
+            persona = new Persona(id, dni, edad, salario);
+
+        } catch (EOFException ex) {
+            ex.printStackTrace();
+            System.out.println("Se ha producido una excepción: " + ex.getMessage());
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.out.println("Se ha producido una excepción: " + ex.getMessage());
+        }
+
+        return persona;
+    }
+
+    private long converToBytePosition(int posicion) {
+        if (posicion == 0) {
+            return posicion;
+        } else {
+            return LONG_BYTES_PERSONA * (posicion - 1);
+        }
+    }
+
+    public Persona add(int posicion, String ruta, Persona persona) {
+        try (
+                 RandomAccessFile raf = new RandomAccessFile(ruta, "rw");) {
+
+            raf.seek(converToBytePosition(posicion));
+
+            raf.writeLong(persona.getId());
+            StringBuilder sb = new StringBuilder(persona.getDni());
+            sb.setLength(9);
+            raf.writeChars(sb.toString());
+            //raf.writeUTF(sb.toString());
+
+            raf.writeInt(persona.getEdad());
+            raf.writeFloat(persona.getSalario());
+
+        } catch (FileNotFoundException ex) {
+            persona = null;
+            ex.printStackTrace();
+            System.out.println("Se ha producido una excepción: " + ex.getMessage());
+        } catch (IOException ex) {
+            persona = null;
+            ex.printStackTrace();
+            System.out.println("Se ha producido una excepción: " + ex.getMessage());
+        }
+        return persona;
+    }
 }
